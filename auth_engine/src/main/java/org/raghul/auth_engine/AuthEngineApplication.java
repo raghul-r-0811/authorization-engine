ca440ba65c5bf3bf3757f7493d070af9f40485ec
raghulramar <raghulramar0811@gmail.com>
2026-01-06 17:08:36 +0000

    Initial Setup Done. Let's do this!!!!.

---
+++ auth_engine/src/main/java/org/raghul/auth_engine/AuthEngineApplication.java
@@ -0,0 +1,13 @@
+package org.raghul.auth_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthEngineApplication.class, args);
+	}
+
+}
---
+++ auth_engine/src/test/java/org/raghul/auth_engine/AuthEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.raghul.auth_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
